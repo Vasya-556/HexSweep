9b9710e67cc8718721fd8fd4472caf86c67c0154
Vasya-556 <vkobrin556@gmail.com>
2025-01-23 18:32:15 +0200

    add hexagons grid and choice between flat and pointy top hexagons

--- main.py
+++ main.py
@@ -7,13 +7,16 @@ pygame.display.set_caption("HexSweep.py")
 
 running = True
 
-def draw_hexagon(Surface, color, size, position):
+def draw_hexgon(Surface, color, size, position, is_flat_top):
     PI = 3.14
     points = []
 
     i = 0
     for j in range(6):
-        angle_deg = 60 * i
+        if is_flat_top:
+            angle_deg = 60 * i
+        else:
+            angle_deg = 60 * i - 30
         angle_rad = PI / 180 * angle_deg
         x = position[0] + size * cos(angle_rad)
         y = position[1] + size * sin(angle_rad)
@@ -27,20 +30,40 @@ def draw_hexagon(Surface, color, size, position):
         points
     )
 
-def draw_hexagon_grid(Surface, color, size, n, start_pos):
+def draw_hexagon_grid(Surface, color, size, n, start_pos, is_flat_top=True):
     height = sqrt(3) * size
     width = 2 * size
-    horiz = 3/2 * size
-    vert = sqrt(3) * size
-    draw_hexagon(Surface, color, size, start_pos)
+    if is_flat_top:    
+        horiz = 3/2 * size
+        vert = sqrt(3) * size
+        vert = vert/2
+    else:
+        horiz = sqrt(3) * size
+        horiz = horiz/2
+        vert = 3/2 * size
 
+    pos_x = start_pos[0]
+    pos_y = start_pos[1]
+
+    for i in range(n):
+        for j in range(n):
+            draw_hexgon(Surface, color, size, (pos_x, pos_y), is_flat_top)
+            pos_x += horiz * 2
+            pos_x -= horiz * 2
+            pos_x += horiz
+            pos_y += vert
+            draw_hexgon(Surface, color, size, (pos_x, pos_y), is_flat_top)
+            pos_x += horiz
+            pos_y -= vert
+        pos_y += vert * 2
+        pos_x = start_pos[0]
 
 while running:
 
     screen.fill("white")
 
     # draw_hexagon(screen, "green", 50, (100,100), 60)
-    draw_hexagon_grid(screen, "blue", 50, 5, (100,100))
+    draw_hexagon_grid(screen, "blue", 50, 3, (100,100),False)
 
     pygame.display.update()
 
