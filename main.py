5bc92ae922bf801d59da2ec30c01721638fb1f34
Vasya-556 <vkobrin556@gmail.com>
2025-01-24 22:30:07 +0200

    added generating mines

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 from math import cos, sin, sqrt
+import random
 
 pygame.init()
 screen = pygame.display.set_mode((600, 400))
 pygame.display.set_caption("HexSweep.py")
 
 running = True
+is_first_hexagon_clicked = False
 
 def draw_hexgon(Surface, color, size, position, is_flat_top):
     PI = 3.14
@@ -47,21 +49,6 @@ def draw_hexagon_grid(Surface, color, size, rows, columns, start_pos, is_flat_to
     pos_x = start_pos[0]
     pos_y = start_pos[1]
 
-    # for row in range(n):
-    #     for col in range(n):
-    #         points = draw_hexgon(Surface, color, size, (pos_x, pos_y), is_flat_top)
-    #         hexagons.append(((row, col), (pos_x, pos_y), points))
-    #         pos_x += horiz * 2
-    #         pos_x -= horiz * 2
-    #         pos_x += horiz
-    #         pos_y += vert
-    #         points = draw_hexgon(Surface, color, size, (pos_x, pos_y), is_flat_top)
-    #         hexagons.append(((row, col), (pos_x, pos_y), points))
-    #         pos_x += horiz
-    #         pos_y -= vert
-    #     pos_y += vert * 2
-    #     pos_x = start_pos[0]
-    
     for row in range(rows):
         for col in range(columns):
             points = draw_hexgon(Surface, color, size, (pos_x, pos_y), is_flat_top)
@@ -92,13 +79,35 @@ def point_in_hexagon(point, hexagon):
         p1x, p1y = p2x, p2y
     return inside
 
+def generate_mines(row, col, first_coord):
+    all_hexagons = row * col
+    all_hexagons_fraction = int(all_hexagons * 0.2)
+    first_coord_row = first_coord[0]
+    first_coord_col = first_coord[1]
+    matrix = [[0] * col for _ in range(row)]
+    mines_remaining = all_hexagons_fraction
+
+    while True:
+        x = random.randrange(0, row)
+        y = random.randrange(0, col)
+        if x == first_coord_row and y == first_coord_col:
+            continue
+        if matrix[x][y] != 1:
+            matrix[x][y] = 1
+            mines_remaining -= 1
+        if mines_remaining <= 0:
+            break
+    
+    print(matrix)
+
 while running:
 
     screen.fill("white")
 
-    # draw_hexagon(screen, "green", 50, (100,100), 60)
-    hexagons = draw_hexagon_grid(screen, "blue", 50, 7, 3, (50,50),False)
-
+    row = 5
+    col = 6
+    hexagons = draw_hexagon_grid(screen, "blue", 50, row, col, (50,50),False)
+    
     pygame.display.update()
 
     for event in pygame.event.get():
@@ -112,4 +121,8 @@ while running:
                 for coords, hex_pos, hex_points in hexagons:
                     if point_in_hexagon(mouse_pos, hex_points):
                         print("hex at", coords)
+                        if not is_first_hexagon_clicked:
+                            generate_mines(row, col, coords)
+                        
+                        is_first_hexagon_clicked = True
                         break
